3711d01f7ae656526e1e81f6ae91633af7181866
Michał Sokołowski <michal.sokolowski95@gmail.com>
2022-06-25 07:44:02 +0200

    Changed files structure

---
+++ frontend/src/components/BrandsList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <v-list dense>
+            <v-subheader>Marki</v-subheader>
+            <v-list-item-group
+                v-model="selectedItem"
+                color="primary"
+            >
+                <v-list-item
+                    v-for="brand in brandsArray"
+                    :key="brand.id"
+                >
+                <v-list-item-icon>
+                    <v-icon>mdi-pistol</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title v-text="brand.name"></v-list-item-title>
+                </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+            </v-list>
+    </div>
+</template>
+
+<script>
+export default  {
+    name: 'BrandsList',
+    data: () =>  ({
+        brandsArray: [
+            {
+                'id': 0,
+                'name': 'Beretta',
+            },
+            {
+                'id': 1,
+                'name': 'Browning'
+            },
+            {
+                'id': 2,
+                'name': 'Heckler&Koch'
+            }
+        ]
+    })
+
+}
+
+</script>
